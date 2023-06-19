9c0d90e22bee113033d3ec4a087eb2336476f94a
root <root@DESKTOP-N38J54P>
2023-06-19 20:41:22 +0100

    fix:eslint

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import StateContext from '../Context/StateContext'
 import '../styles/globals.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 import { appWithTranslation } from 'next-i18next'
@@ -15,31 +15,12 @@ const MyApp = ({ Component, pageProps }) => {
         duration: 1500,
         // once: true,
         easing: 'ease-in-out',
-        mirror: true
-
+        mirror: true,
       })
     }
   }, [])
-    // const [isLoading, setIsLoading] = useState(true)
-
-    // useEffect(() => {
-    //   const timeout = setTimeout(() => {
-    //     setIsLoading(false)
-    //   }, 4000)
-
-    //   return () => {
-    //     clearTimeout(timeout)
-    //   }
-    // }, [])
-    // if (isLoading) {
-    //   // render your loading page
-    //   return (
-    //     <div className=' flex absolute box-border z-50 bg-slate-400 top-0 left-0 w-screen h-screen' />
-    //   )
-    // }
   return (
     <StateContext>
-      
       <Layout>
         <ToastContainer
           position='top-center'
